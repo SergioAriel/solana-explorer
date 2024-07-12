e79c4cca3b606ab5bbd7efd8bebe7797831efa21
Sergio Solis <sergiosolis@MacBook-Pro-de-Sergio.local>
2024-07-12 20:55:49 -0300

    add tokes fungibles

--- pages/[...search]/index.tsx
+++ pages/[...search]/index.tsx
@@ -7,7 +7,6 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Address({ data }: any) {
   const router = useRouter()
-  const [tabSelected, setTabSelected] = useState<string>('')
   console.log(data)
   return (
     <>
@@ -24,108 +23,160 @@ export default function Address({ data }: any) {
             <span>SOL</span>
             <span>
               {
-                data?.infoAddress?.nativeBalance?.lamports / 1000000000
+                data?.balance?.nativeBalance?.lamports / 1000000000
               }
             </span>
-            <span>$ {data?.infoAddress?.nativeBalance?.total_price.toFixed(6)}</span>
+            <span>$ {data?.balance?.nativeBalance?.total_price.toFixed(6)}</span>
           </div>
+
         </div>
       </div>
 
-      <div className="flex flex-col bg-white w-full mb-6 shadow-lg rounded ">
-        <div className="rounded-t mb-0 px-4 py-3 border-0">
-          <div className="flex flex-wrap items-center">
-            <div className="flex justify-between w-full px-4 max-w-full flex-grow flex-1">
-              <h3 className="font-semibold text-base text-blueGray-700">Transactions</h3>
-              <button
-                onClick={() => {
-
-                  console.log([router?.query?.search?.[0], data?.transactions[data?.transactions?.length - 1].signature])
-                  router.push({
-                    pathname: `/transactions/[...transactions]`,
-                    query: {
-                      transactions: [router?.query?.search?.[0], data?.transactions[data?.transactions?.length - 1].signature]
-                    }
-                  })
-                }}
+      <div
+        className="flex gap-10"
+      >
+        <div className="flex flex-col bg-white w-1/2 h-96 overflow-y-auto shadow-lg rounded ">
+          <div className="flex flex-col w-full max-w-full flex-grow flex-1">
+            <div className="flex justify-between w-full px-4 h-12 items-center">
+              <h3 className="font-semibold text-base text-gray-700">Transactions</h3>
+            </div>
+            <div
+              className="flex flex-col"
+            >
+              <table
+                className="w-full"
               >
-                SeeAll
-              </button>
+                <thead>
+                  <tr
+                    className="bg-gray-200 text-gray-600 border border-solid"
+                  >
+                    <th>symbol</th>
+                    <th>balance</th>
+
+                  </tr>
+                </thead>
+                <tbody>
+                  {
+                    data?.balance?.items?.map((item: any) => {
+                      if (item.token_info.price_info) {
+
+                        console.log(10 ^ item.token_info.decimals)
+                        return (
+                          <tr
+                            key={item.id}
+                            className="[&>:first-child]:text-black text-gray-500"
+                          >
+                            <th>{item.token_info.symbol}</th>
+                            <th>{(item.token_info.balance / (10 ** item.token_info.decimals)).toFixed(5)}</th>
+
+                          </tr>
+                        )
+                      }
+                    })
+                  }
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
-        <table className="items-center bg-transparent w-full">
-          <thead
-            className="block w-full"
-          >
-            <tr
-              className="table table-fixed w-full"
-            >
-              <th className="px-6 bg-gray-200 text-gray-600  border border-solid py-3 text-xs uppercase border-l-0 border-r-0 text-ellipsis font-semibold ">
-                Signature
-              </th>
-              <th className="px-6 bg-gray-200 text-gray-600  border border-solid py-3 text-xs uppercase border-l-0 border-r-0 text-ellipsis font-semibold ">
-                Block
-              </th>
-              <th className="px-6 bg-gray-200 text-gray-600  border border-solid py-3 text-xs uppercase border-l-0 border-r-0 text-ellipsis font-semibold ">
-                Time
-              </th>
-              <th className="px-6 bg-gray-200 text-gray-600  border border-solid py-3 text-xs uppercase border-l-0 border-r-0 text-ellipsis font-semibold ">
-                By
-              </th>
-            </tr>
-          </thead>
 
-          <tbody
-            className="block w-full"
-          >
-            {
-              data?.transactions && data?.transactions?.map((txn: any) => {
 
-                const time = new Date(txn.timestamp * 1000);
-                const fullTime = time.toUTCString();
+        <div className="flex flex-col bg-white w-1/2  h-96 overflow-y-auto shadow-lg rounded ">
+          <div className=" sticky top-0 bg-white rounded-t h-12  px-4 py-3 border-0">
+            <div className="flex flex-wrap items-center">
+              <div className="flex justify-between w-full px-4 max-w-full flex-grow flex-1">
+                <h3 className="font-semibold text-base text-gray-700">Transactions</h3>
+                <button
+                  onClick={() => {
 
-                return (
-                  <tr
-                    key={txn.signature}
-                    className={`table table-fixed w-full text-left [&>:first-child]:text-black text-gray-500`}
-                  >
-                    <th
-                      className=" border-t-0 truncate px-6 align-middle border-l-0 border-r-0 text-xs p-4 "
-
-                    >
-                      {txn.signature}
-                    </th>
-                    <th
-                      className=" border-t-0  truncate px-6 align-middle border-l-0 border-r-0 text-xs p-4 "
-
-                    >
-                      {txn.slot}
-                    </th>
-                    <th
-                      className=" border-t-0  truncate px-6 align-middle border-l-0 border-r-0 text-xs p-4 "
-
-                    >
-                      {/* { `${hours}:${minutes}:${seconds} ${month}/${date}/${year}` } */}
-                      {fullTime}
-                    </th>
-                    <th
-                      className=" border-t-0  truncate px-6 align-middle border-l-0 border-r-0 text-xs p-4 "
-
-                    >
-                      {
-                        txn.feePayer
+                    console.log([router?.query?.search?.[0], data?.transactions[data?.transactions?.length - 1].signature])
+                    router.push({
+                      pathname: `/transactions/[...transactions]`,
+                      query: {
+                        transactions: [router?.query?.search?.[0], data?.transactions[data?.transactions?.length - 1].signature]
                       }
-                    </th>
-                  </tr>
-                )
-              })
-            }
-          </tbody>
+                    })
+                  }}
+                >
+                  SeeAll
+                </button>
+              </div>
+            </div>
+          </div>
+          <table className="items-center bg-transparent w-full">
+            <thead
+              className="sticky top-12 w-full"
+            >
+              <tr
+                className="table table-fixed w-full bg-gray-200 text-gray-600 border border-solid"
+              >
+                <th className="px-6 py-3 text-xs uppercase border-l-0 border-r-0 text-ellipsis font-semibold ">
+                  Signature
+                </th>
+                <th className="px-6 py-3 text-xs uppercase border-l-0 border-r-0 text-ellipsis font-semibold ">
+                  Block
+                </th>
+                <th className="px-6 py-3 text-xs uppercase border-l-0 border-r-0 text-ellipsis font-semibold ">
+                  Time
+                </th>
+                <th className="px-6 py-3 text-xs uppercase border-l-0 border-r-0 text-ellipsis font-semibold ">
+                  By
+                </th>
+              </tr>
+            </thead>
 
-        </table>
+            <tbody
+              className="block w-full"
+            >
+              {
+                data?.transactions && data?.transactions?.map((txn: any) => {
 
+                  const time = new Date(txn.timestamp * 1000);
+                  const fullTime = time.toUTCString();
+
+                  return (
+                    <tr
+                      key={txn.signature}
+                      className={`table table-fixed w-full text-left [&>:first-child]:text-black text-gray-500`}
+                    >
+                      <th
+                        className=" border-t-0 truncate px-6 align-middle border-l-0 border-r-0 text-xs p-4 "
+
+                      >
+                        {txn.signature}
+                      </th>
+                      <th
+                        className=" border-t-0  truncate px-6 align-middle border-l-0 border-r-0 text-xs p-4 "
+
+                      >
+                        {txn.slot}
+                      </th>
+                      <th
+                        className=" border-t-0  truncate px-6 align-middle border-l-0 border-r-0 text-xs p-4 "
+
+                      >
+                        {/* { `${hours}:${minutes}:${seconds} ${month}/${date}/${year}` } */}
+                        {fullTime}
+                      </th>
+                      <th
+                        className=" border-t-0  truncate px-6 align-middle border-l-0 border-r-0 text-xs p-4 "
+
+                      >
+                        {
+                          txn.feePayer
+                        }
+                      </th>
+                    </tr>
+                  )
+                })
+              }
+            </tbody>
+
+          </table>
+
+        </div>
       </div>
+
     </>
   );
 }
@@ -144,7 +195,8 @@ export const getStaticProps = (async ({ params }) => {
 
 
   const infoAddress = async () => {
-    const responseBalance = await fetch(`https://mainnet.helius-rpc.com/?api-key=${process.env.API_KEY}`, {
+    const url = `https://mainnet.helius-rpc.com/?api-key=${process.env.API_KEY}`;
+    const response = await fetch(url, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -152,38 +204,41 @@ export const getStaticProps = (async ({ params }) => {
       body: JSON.stringify({
         jsonrpc: '2.0',
         id: 'my-id',
-        method: 'getAssetsByOwner',
+        method: 'searchAssets',
         params: {
           ownerAddress: search?.[0],
-          page: 1, // Starts at 1
-          limit: 50,
+          tokenType: 'fungible',
           displayOptions: {
-            showFungible: true, //return both fungible and non-fungible tokens
             showNativeBalance: true,
-          }
+          },
+          page: 1
         },
       }),
     });
-    const { result: infoAddress } = await responseBalance.json();
-    if(infoAddress){
+    const { result: balance } = await response.json();
+
+    console.log(balance)
+
+
+    if (balance) {
       const urlTransaction = `https://api.helius.xyz/v0/addresses/${search?.[0]}/transactions?api-key=${process.env.API_KEY}`
       console.log(urlTransaction)
       const responseTransactions = await fetch(urlTransaction, {
         method: 'GET',
       });
       const transactions = await responseTransactions.json();
-      if(transactions.length){
+      if (transactions.length) {
         const data = {
           type: "address",
           transactions,
-          infoAddress
+          balance
         }
         return { props: { data } }
       }
     }
     const data = {
       type: "address",
-      infoAddress
+      balance
     }
     return { props: { data } }
   }
